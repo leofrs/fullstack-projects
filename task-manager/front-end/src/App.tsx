ca6a8b028f8495b951d1200b060c6a9203964316
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-09-13 14:41:05 -0300

    front-end modificado

--- task-manager/front-end/src/App.tsx
+++ task-manager/front-end/src/App.tsx
@@ -1,15 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
 import RouterIndex from "./routes";
-import { UserProvider } from "./context/userContext";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <UserProvider>
-        <RouterIndex />
-      </UserProvider>
-    </BrowserRouter>
-  );
+  return <RouterIndex />;
 }
 
 export default App;
