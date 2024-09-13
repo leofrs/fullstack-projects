36c2d9e574ab16cc95dae532a6b514ab627480c3
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-09-13 10:43:25 -0300

    Versão 1.0: cors polity updated

--- task-manager/back-end/src/index.ts
+++ task-manager/back-end/src/index.ts
@@ -14,6 +14,7 @@ app.use(
     origin: "https://task-manager-front-end-kohl.vercel.app",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
   })
 );
 app.use(express.json());
